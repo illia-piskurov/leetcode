970e371ad787d00ecd484b89adb36c6687f5c97a
Ilya Piskurov <piskryov@gmail.com>
2023-10-16 01:14:08 +0300

    add 26

---
+++ easy/26_remove_duplicates_from_sorted_array/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let len = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[i - 1]) {
+            nums[len] = nums[i];
+            len++; 
+        }
+    }
+    return len;
+};
